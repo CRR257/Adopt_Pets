5bea87b50f7ac74e6bda41a3c85ab098c24a4afb
lluna <lluna@example.com>
2019-07-01 21:01:20 +0200

    V1- WIP - add pet class

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React from "react"
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import {render} from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
     alert("you clicket the title");
   }
   render() {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Jofre",
-        animal: "cat",
-        breed: "british"
-      }),
-      React.createElement(Pet, {
-        name: "Gal.la",
-        animal: "cat",
-        breed: "british"
-      }),
-      React.createElement(Pet, {
-        name: "Marilyn",
-        animal: "turtle",
-        breed: "russian"
-      })
-    );
+
+  return (
+    <div>
+    <h1 onClick={this.handleTitleClick()}>Adopt me!</h1>
+    <Pet name="Jofre" animal="cat" breed="british" />
+    <Pet name="Jofre" animal="cat" breed="british" />
+    <Pet name="Jofre" animal="cat" breed="british" />
+    </div>
+  )
   }
+
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
